83309df4ab30c5ec0d0814bf677a12d6e4f8434c
FelipeAPM <35493876+casfe03@users.noreply.github.com>
2022-03-28 20:12:38 -0300

    Update hello-openshift.go

--- hello-openshift.go
+++ hello-openshift.go
@@ -14,7 +14,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		helloEnv = "hello"
 	}
 
-	arg := "openshift"
+	arg := "openshift e Red Academu"
 	if len(os.Args) > 1 {
 		arg = os.Args[1]
 	}
